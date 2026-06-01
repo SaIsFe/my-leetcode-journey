c5d49ef8a0b147f96cf3d858757057193a3e2e4c
Shanta Islam Ferdausi <shanta20154@gmail.com>
2026-06-01 00:03:40 -0700

    Time: 20 ms (47.85%), Space: 28.5 MB (58.22%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string ans="";
+        for(auto x:s){
+            if(x!='*')ans.push_back(x);
+            else ans.pop_back();
+        }
+        return ans;
+    }
+};
